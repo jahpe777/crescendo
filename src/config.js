1473679d8c1a60aae3144eea2bba825bd809942a
James Higgs <james.y.higgs@gmail.com>
2020-04-02 08:00:01 -0700

    200402_1

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:7000',
-  /*`https://powerful-basin-50906.herokuapp.com`*/
+  API_ENDPOINT: 'https://powerful-basin-50906.herokuapp.com',
 
-  TOKEN_KEY: 'crescendo-client-auth-token'
+  REACT_APP_TOKEN_KEY:
+    process.env.REACT_APP_TOKEN_KEY || 'crescendo-client-auth-token'
 };
